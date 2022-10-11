1b18be96afb660e6dd342df3c9a05d01814742b1
anqi <anqix2002@gmail.com>
2022-10-10 17:20:38 -0700

    Deleted flask mail

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import (StringField, TextAreaField, IntegerField, BooleanField, RadioField, SubmitField)
-from wtforms.validators import InputRequired, Length
-
-class ContactForm(FlaskForm):
-    username = StringField('ENTER YOUR EMAIL', validators=[InputRequired(), Length(min=5, max=100)])
-    query = TextAreaField('ASK ANQI', validators=[InputRequired(), Length(max=500)])
-    submit = SubmitField('Submit')
